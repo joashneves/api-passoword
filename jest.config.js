5334b6e259a2d4bd435ba0d24b2184dbee735959
joashneves <joashneves@gmail.com>
2025-08-20 21:07:55 -0300

    migration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,18 @@
 const dotenv = require("dotenv");
-dotenv.config({
-  path: ".env.development",
-});
+dotenv.config({ path: ".env.development" });
 
 const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  dir: ".",
-});
+const createJestConfig = nextJest({ dir: "." });
 
 const jestConfig = createJestConfig({
+  preset: "ts-jest/presets/default-esm",
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
   moduleDirectories: ["node_modules", "<rootDir>"],
+  extensionsToTreatAsEsm: [".ts"],
   testTimeout: 600000,
 });
 
---
+++ pages/api/v1/migrations/index.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import migrator from "@/models/migrator";
+
+export default async function handler(req:NextApiRequest, res: NextApiResponse) {
+  if(req.method === 'POST'){
+    postHandler(req, res);
+  }else if (req.method === 'GET'){
+    getHandler(req, res)
+  }
+  else{
+   return res.status(405).json({ message: "Method not allowed" });
+
+  }
+  
+}
+
+async function getHandler(req: NextApiRequest, res: NextApiResponse) {
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return res.status(200).json(pendingMigrations);
+}
+
+async function postHandler(req: NextApiRequest , res: NextApiResponse) {
+  const migratedMigrations = await migrator.runPendingMigrations();
+
+  if (migratedMigrations.length > 0) {
+    return res.status(201).json(migratedMigrations);
+  }
+
+  return res.status(200).json(migratedMigrations);
+}
